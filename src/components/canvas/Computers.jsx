62bf533ccaa92d24e6309df8c0e161f9f390ede3
ahmed abdelmageed <ahmedabdelmageed121@gmail.com>
2023-06-24 22:20:36 +0300

    changes

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,28 +5,27 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./planet/scene.gltf");
+  const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
-    ggggggggg
-    // <mesh>
-    //   <hemisphereLight intensity={0.15} groundColor='black' />
-    //   <spotLight
-    //     position={[-20, 50, 10]}
-    //     angle={0.12}
-    //     penumbra={1}
-    //     intensity={1}
-    //     castShadow
-    //     shadow-mapSize={1024}
-    //   />
-    //   <pointLight intensity={1} />
-    //   <primitive
-    //     object={computer.scene}
-    //     scale={isMobile ? 0.65 : 0.75}
-    //     position={isMobile ? [0, -3.1, -2.2] : [0, -3.25, -1.5]}
-    //     rotation={[-0.01, -0.2, -0.1]}
-    //   />
-    // </mesh>
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor='black' />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <pointLight intensity={1} />
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 0.65 : 0.75}
+        position={isMobile ? [0, -3.1, -2.2] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
+    </mesh>
   );
 };
 
