47fc0bd9d7d2040825121db34462180b3235eddf
ahmed abdelmageed <ahmedabdelmageed121@gmail.com>
2023-06-24 22:16:31 +0300

    test

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -8,24 +8,25 @@ const Computers = ({ isMobile }) => {
   const computer = useGLTF("./planet/scene.gltf");
 
   return (
-    <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
-      <spotLight
-        position={[-20, 50, 10]}
-        angle={0.12}
-        penumbra={1}
-        intensity={1}
-        castShadow
-        shadow-mapSize={1024}
-      />
-      <pointLight intensity={1} />
-      <primitive
-        object={computer.scene}
-        scale={isMobile ? 0.65 : 0.75}
-        position={isMobile ? [0, -3.1, -2.2] : [0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
-      />
-    </mesh>
+    ggggggggg
+    // <mesh>
+    //   <hemisphereLight intensity={0.15} groundColor='black' />
+    //   <spotLight
+    //     position={[-20, 50, 10]}
+    //     angle={0.12}
+    //     penumbra={1}
+    //     intensity={1}
+    //     castShadow
+    //     shadow-mapSize={1024}
+    //   />
+    //   <pointLight intensity={1} />
+    //   <primitive
+    //     object={computer.scene}
+    //     scale={isMobile ? 0.65 : 0.75}
+    //     position={isMobile ? [0, -3.1, -2.2] : [0, -3.25, -1.5]}
+    //     rotation={[-0.01, -0.2, -0.1]}
+    //   />
+    // </mesh>
   );
 };
 
