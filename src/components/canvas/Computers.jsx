295d419f5ed147d6c365a592beb5666f076fe13d
ahmed abdelmageed <ahmedabdelmageed121@gmail.com>
2023-06-25 23:26:25 +0300

    changes

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -56,14 +56,14 @@ const ComputersCanvas = () => {
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
-      <Suspense fallback={<CanvasLoader />}>
+      {/* <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
         <Computers isMobile={isMobile} />
-      </Suspense>
+      </Suspense> */}
 
       <Preload all />
     </Canvas>
