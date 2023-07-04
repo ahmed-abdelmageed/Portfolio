9bb772b4d3b98d3ee72566057d6f4d3889ef6739
ahmed abdelmageed <ahmedabdelmageed121@gmail.com>
2023-07-04 21:41:15 +0300

    changes

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -3,27 +3,23 @@ import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 import { styles } from "../styles";
-
+import { jsx as _jsx } from "react/jsx-runtime";
+import { Fragment as _Fragment } from "react/jsx-runtime";
+import { jsxs as _jsxs } from "react/jsx-runtime";
 const Tech = () => {
-  return (
-       <><h2 className={`${styles.sectionHeadText} text-center`}>
-      Technologis
-    </h2>
-      <br/>
-      <br/>
-      <div className='flex flex-row flex-wrap justify-center gap-10'>
-
-        {technologies.map((technology) => (
-          <div className='w-28 h-28' key={technology.name}>
-            <BallCanvas icon={technology.icon} />
-          </div>
-        ))}
-      </div>
-    <br/>
-    <br/>
-    <br/>
-    </>
-  );
-}
-
-export default Tech
+  return /*#__PURE__*/_jsxs(_Fragment, {
+    children: [/*#__PURE__*/_jsx("h2", {
+      className: `${styles.sectionHeadText} text-center`,
+      children: "Technologis"
+    }), /*#__PURE__*/_jsx("br", {}), /*#__PURE__*/_jsx("br", {}), /*#__PURE__*/_jsx("div", {
+      className: "flex flex-row flex-wrap justify-center gap-10",
+      children: technologies.map(technology => /*#__PURE__*/_jsx("div", {
+        className: "w-28 h-28",
+        children: /*#__PURE__*/_jsx(BallCanvas, {
+          icon: technology.icon
+        })
+      }, technology.name))
+    }), /*#__PURE__*/_jsx("br", {}), /*#__PURE__*/_jsx("br", {}), /*#__PURE__*/_jsx("br", {})]
+  });
+};
+export default Tech;
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,73 +1,70 @@
 import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
-
 import CanvasLoader from "../Loader";
-
-const Computers = ({ isMobile }) => {
+import { jsx as _jsx } from "react/jsx-runtime";
+import { jsxs as _jsxs } from "react/jsx-runtime";
+const Computers = ({
+  isMobile
+}) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
-
-  return (
-    <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
-      <spotLight
-        position={[-20, 50, 10]}
-        angle={0.12}
-        penumbra={1}
-        intensity={1}
-        castShadow
-        shadow-mapSize={1024}
-      />
-      <pointLight intensity={1} />
-      <primitive
-        object={computer.scene}
-        scale={isMobile ? 0.65 : 0.75}
-        position={isMobile ? [0, -3.1, -2.2] : [0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
-      />
-    </mesh>
-  );
+  return /*#__PURE__*/_jsxs("mesh", {
+    children: [/*#__PURE__*/_jsx("hemisphereLight", {
+      intensity: 0.15,
+      groundColor: "black"
+    }), /*#__PURE__*/_jsx("spotLight", {
+      position: [-20, 50, 10],
+      angle: 0.12,
+      penumbra: 1,
+      intensity: 1,
+      castShadow: true,
+      "shadow-mapSize": 1024
+    }), /*#__PURE__*/_jsx("pointLight", {
+      intensity: 1
+    }), /*#__PURE__*/_jsx("primitive", {
+      object: computer.scene,
+      scale: isMobile ? 0.65 : 0.75,
+      position: isMobile ? [0, -3.1, -2.2] : [0, -3.25, -1.5],
+      rotation: [-0.01, -0.2, -0.1]
+    })]
+  });
 };
-
 const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
-
   useEffect(() => {
     const mediaQuery = window.matchMedia("(max-width: 500px)");
-
     setIsMobile(mediaQuery.matches);
-
-    const handleMediaQueryChange = (event) => {
+    const handleMediaQueryChange = event => {
       setIsMobile(event.matches);
     };
-
     mediaQuery.addEventListener("change", handleMediaQueryChange);
-
     return () => {
       mediaQuery.removeEventListener("change", handleMediaQueryChange);
     };
   }, []);
-
-  return (
-    <Canvas
-      frameloop="demand"
-      shadows
-      dpr={[1, 2]}
-      camera={{ position: [20, 3, 5], fov: 25 }}
-      gl={{ preserveDrawingBuffer: true }}
-    >
-      <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls
-          enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-        />
-        <Computers isMobile={isMobile} />
-      </Suspense>
-
-      <Preload all />
-    </Canvas>
-  );
+  return /*#__PURE__*/_jsxs(Canvas, {
+    frameloop: "demand",
+    shadows: true,
+    dpr: [1, 2],
+    camera: {
+      position: [20, 3, 5],
+      fov: 25
+    },
+    gl: {
+      preserveDrawingBuffer: true
+    },
+    children: [/*#__PURE__*/_jsxs(Suspense, {
+      fallback: /*#__PURE__*/_jsx(CanvasLoader, {}),
+      children: [/*#__PURE__*/_jsx(OrbitControls, {
+        enableZoom: false,
+        maxPolarAngle: Math.PI / 2,
+        minPolarAngle: Math.PI / 2
+      }), /*#__PURE__*/_jsx(Computers, {
+        isMobile: isMobile
+      })]
+    }), /*#__PURE__*/_jsx(Preload, {
+      all: true
+    })]
+  });
 };
-
 export default ComputersCanvas;
