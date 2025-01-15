107d85241a9d518fe0eefb72d349b0772b7f1f92
ahmed-abdelmageed <58009386+ahmed-abdelmageed@users.noreply.github.com>
2025-01-15 04:57:13 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
-import { About, Contact, Experience, Hero, Navbar, Tech, Works } from "./components";
-import { checkWebGLSupport } from "./utils/checkWebGLSupport";
+
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
-  // Check if WebGL is supported
-  const isWebGLSupported = checkWebGLSupport();
-
   return (
     <BrowserRouter>
-      <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
           <Hero />
         </div>
@@ -17,24 +14,14 @@ const App = () => {
         <Experience />
         <Tech />
         <Works />
-        <div className="relative z-0">
-          {isWebGLSupported ? (
-            <>
-              {/* Uncomment this line when ready */}
-              {/* <StarsCanvas /> */}
-              <Contact />
-            </>
-          ) : (
-            <div className="bg-gray-800 text-white text-center py-4">
-              <p>Your browser or device does not support WebGL.</p>
-              <p>For the best experience, please use a different browser or device.</p>
-            </div>
-          )}
+         <div className='relative z-0'>
+          {/* <StarsCanvas /> */}
+        <Contact />
         </div>
         {/* <Feedbacks /> */}
       </div>
     </BrowserRouter>
   );
-};
+}
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    terserOptions: {
-      compress: {
-        drop_console: true, 
-        drop_debugger: true, 
-      },
-      format: {
-        comments: false, 
-      },
-    },
+    target: 'es2015'
   },
 });
