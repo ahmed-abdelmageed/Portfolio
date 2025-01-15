195813c46adf5573791969bd14f3ed5f4c38155c
ahmed-abdelmageed <58009386+ahmed-abdelmageed@users.noreply.github.com>
2025-01-15 04:47:43 +0200

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
         </div>
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi , I'm <span className='text-[#915eff]'> Ahmed f </span>
+            Hi , I'm <span className='text-[#915eff]'> Ahmed</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`} > I develop user interfaces and web applications  </p>
         </div>
