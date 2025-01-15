fd293414f3c9af690aacccb3131e40a264e5b065
ahmed-abdelmageed <58009386+ahmed-abdelmageed@users.noreply.github.com>
2025-01-15 04:11:49 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter } from "react-router-dom";
-
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import { About, Contact, Experience, Hero, Navbar, Tech, Works } from "./components";
+import { checkWebGLSupport } from "./utils/checkWebGLSupport";
 
 const App = () => {
+  // Check if WebGL is supported
+  const isWebGLSupported = checkWebGLSupport();
+
   return (
     <BrowserRouter>
-      <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
         </div>
@@ -14,14 +17,25 @@ const App = () => {
         <Experience />
         <Tech />
         <Works />
-         <div className='relative z-0'>
-          {/* <StarsCanvas /> */}
-        <Contact />
+        <div className="relative z-0">
+          {/* Conditionally render StarsCanvas based on WebGL support */}
+          {isWebGLSupported ? (
+            <>
+              {/* Uncomment this line when ready */}
+              {/* <StarsCanvas /> */}
+              <Contact />
+            </>
+          ) : (
+            <div className="bg-gray-800 text-white text-center py-4">
+              <p>Your browser or device does not support WebGL.</p>
+              <p>For the best experience, please use a different browser or device.</p>
+            </div>
+          )}
         </div>
         {/* <Feedbacks /> */}
       </div>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
