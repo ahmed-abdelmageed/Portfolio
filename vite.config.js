ee8ce6ae23dae14d2e79177b31ea972231c34107
ahmed-abdelmageed <58009386+ahmed-abdelmageed@users.noreply.github.com>
2025-01-15 04:22:59 +0200

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: 'es2015', // Ensures compatibility with older browsers
+    terserOptions: {
+      compress: {
+        drop_console: true, 
+        drop_debugger: true, 
+      },
+      format: {
+        comments: false, 
+      },
+    },
   },
-})
+});
