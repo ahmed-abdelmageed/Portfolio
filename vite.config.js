8641ef02e343d178b55b7298b9e21c93c4465fdc
ahmed-abdelmageed <58009386+ahmed-abdelmageed@users.noreply.github.com>
2025-01-19 15:16:55 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import legacy from "@vitejs/plugin-legacy";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy({
-      targets: ["defaults", "not IE 11"],
-    }),
-  ],
+  plugins: [react()],
+  build: {
+    target: "es2015",
+  },
+  optimizeDeps: {
+    include: ["swiper"],
+  },
 });
